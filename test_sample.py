e5436e2741fc08f4643df7b455ff52c395d5b82d
Hugh Mallinson <hugh.mallinson@gmail.com>
2023-08-18 10:09:29 +0100

    add test_sample

---
+++ test_sample.py
@@ -0,0 +1,31 @@
+import logging
+
+import pandas as pd
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler("debug.log"),
+        logging.StreamHandler()
+    ]
+)
+
+def func(x:int):
+    return x + 1
+
+def ppppp(df:pd.DataFrame):
+    logging.info(df.columns)
+    
+    
+def test_answer():
+    assert func(3) == 4
+    
+
+
+func(3)
+
+df1= pd.DataFrame({'a':[1,1,1,1,1]})
+
+ppppp(df1)
